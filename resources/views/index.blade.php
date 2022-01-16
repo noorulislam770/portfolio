a05b62a09526f5c5de8180be6a36bcaee1c197d6
Noor Ul Islam <noorulislam770@gmail.com>
2022-01-16 22:10:06 +0500

    first commit initial Portfolio

---
+++ resources/views/index.blade.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Noor Ul Islam - Software Developer</title>
+    <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}">
+    {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700"> --}}
+    <link rel="stylesheet" href="{{asset('assets/fonts/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/fonts/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Contact-Form-Clean.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Features-Clean.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Footer-Basic.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Header-Blue.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Highlight-Phone.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Map-Clean.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Navigation-Clean.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Navigation-with-Search.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Newsletter-Subscription-Form.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Projects-Clean.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/styles.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Testimonials.css')}}">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-lg navigation-clean">
+        <div class="container"><a class="navbar-brand" href="#"><img class="rounded-circle img-fluid" src="{{('assets/img/img_square.png')}}" width="80px"></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#reviews">Reviews</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+        <section id="about" class="highlight-phone">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="intro">
+                            <h2>About Me</h2>
+                            <p>Hi! I am Noor, A Tech Enthusiast, mainly interested in Web Software Development For Amazing Entrepreneurs Getting their ideas online.&nbsp; Feel Free To contact Me and share your idea with me and we will together&nbsp; bring your idea to a working product.</p><a class="btn btn-primary" role="button" href="#contact">Book ME Now!</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="d-none d-md-block phone-mockup"><img class="img-fluid device" src="{{('assets/img/IMG_7378.jpg')}}">
+                            <div class="screen"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="projects" class="projects-clean">
+            <div class="container">
+                <div class="intro">
+                    <h2 class="text-center">Projects </h2>
+                    <p class="text-center">These Are Some Of My Projects that I recently developed have a look and build your confidence</p>
+                </div>
+                <div class="row projects">
+                    <div class="col-sm-6 col-lg-4 item"><img class="img-fluid" src="{{('assets/img/desk.jpg')}}">
+                        <h3 class="name">School Student Management</h3>
+                        <p class="description">A Software Project Developed in PHP for Small Budget Schools to handle the admission system.</p>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 item"><img class="img-fluid" src="{{('assets/img/building.jpg')}}">
+                        <h3 class="name">CRUD</h3>
+                        <p class="description">A Simple Demonstration of how to create a simple CRUD application. A application that can Create Read Update and Delete data basically the backbone of every modern full stack application I developed for my students on YouTube.&nbsp;</p>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 item"><img class="img-fluid" src="{{('assets/img/loft.jpg')}}">
+                        <h3 class="name">Gauss Seidel Algorithm</h3>
+                        <p class="description">Aside from web development I also Do python and data science. Created a program for a math problem that can craft a solution to a gauss seidel problem along with the descriptive solutions.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="reviews" class="testimonials-clean">
+            <div class="container">
+                <div class="intro">
+                    <h2 class="text-center">Reviews for Clients And Collegues</h2>
+                    <p class="text-center">Listen to the reviews of many colleagues and friends about the work I have done.&nbsp;</p>
+                </div>
+                <div class="row people">
+                    <div class="col-md-6 col-lg-4 item">
+                        <div class="box">
+                            <p class="description">Probably the best instructor on YouTube I have learned almost all of the things from him. "YouTube Comment"</p>
+                        </div>
+                        <div class="author"><img class="rounded-circle" src="{{('assets/img/1.jpg')}}">
+                            <h5 class="name">Sameer Ahmed</h5>
+                            <p class="title">Computer Science Student and Subscriber</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 item">
+                        <div class="box">
+                            <p class="description">I have a fully mature business. Which would not have been possible without the help of Noor. Seriously very Fortunate To have you as a partner</p>
+                        </div>
+                        <div class="author"><img class="rounded-circle" src="{{('assets/img/3.jpg')}}">
+                            <h5 class="name">Aliyan Khan</h5>
+                            <p class="title">Founder of Healthy Footwear</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 item">
+                        <div class="box">
+                            <p class="description">Your Gauss Seidel algorithm gave me an idea of how to implement any maths equation using&nbsp; python and thanks for that. -YouTube Comment</p>
+                        </div>
+                        <div class="author"><img class="rounded-circle" src="{{('assets/img/2.jpg')}}">
+                            <h5 class="name">Abdur Rehman</h5>
+                            <p class="title">YouTube Subscriber</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="services" class="features-clean">
+            <div class="container">
+                <div class="intro">
+                    <h2 class="text-center" id="services">Services</h2>
+                    <p class="text-center">I am currently providing a bunch of services which are core to getting you started. And in the future we are expanding the team and we will be your one&nbsp; go to software Solution.</p>
+                </div>
+                <div class="row features">
+                    <div class="col-sm-6 col-lg-4 item"><i class="fa fa-map-marker icon"></i>
+                        <h3 class="name">Map Addition Services</h3>
+                        <p class="description">An easy way to include google maps navigation in your old app giving your clients an easier way to navigate to you</p>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 item"><i class="fa fa-clock-o icon"></i>
+                        <h3 class="name">React Developement</h3>
+                        <p class="description">E-commerce businesses development with a complete shopping and payment methods included&nbsp;</p>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 item"><i class="fa fa-list-alt icon"></i>
+                        <h3 class="name">Blogs with feeds</h3>
+                        <p class="description">Create an online presence for you work. journalize your life on your life with your blog and an easy way to send news to your subscribers</p>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 item"><i class="fa fa-leaf icon"></i>
+                        <h3 class="name">Full Stack Web Dev</h3>
+                        <p class="description">You just come with an idea that solves problems. and I will bring&nbsp; your idea to a reality. get up and running in weeks&nbsp;</p>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 item"><i class="fa fa-plane icon"></i>
+                        <h3 class="name">Desktop Software</h3>
+                        <p class="description">Software for your businesses in your own computer no additional setup.&nbsp;&nbsp;</p>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 item"><i class="fa fa-phone icon"></i>
+                        <h3 class="name">Mobile App Dev</h3>
+                        <p class="description">Have an app idea just hop on and see your app coming to reality in weeks.&nbsp;</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="contact" class="contact-clean" style="margin: 2px;margin-top: 39px;">
+            <form method="post">
+                <h2 class="text-center">Book Me</h2>
+                <div class="mb-3"><input class="form-control" type="text" name="name" placeholder="Name"></div>
+                <div class="mb-3"><input class="form-control is-invalid" type="email" name="email" placeholder="Email"></div>
+                <div class="mb-3"><textarea class="form-control" name="message" placeholder="Message" rows="14"></textarea></div>
+                <div class="mb-3"><button class="btn btn-primary" type="submit">send </button></div>
+            </form>
+        </section>
+        <section class="newsletter-subscribe" style="margin-top: 19px;">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Subscribe for My News Later and New Updates</h2>
+                <p class="text-center">Get in Touch to get the latest projects and new frameworks added to the stack. get new deals and get up and running with us.</p>
+            </div>
+            <form class="d-flex justify-content-center flex-wrap" method="post">
+                <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Your Email"></div>
+                <div class="mb-3"><button class="btn btn-primary" type="submit">Subscribe </button></div>
+            </form>
+        </div>
+    </section>
+    <footer class="footer-basic" style="margin-top: 51px;">
+        {{-- <section class="map-clean">
+            <div class="container">
+                <div class="intro">
+                    <h2 class="text-center">Location </h2>
+                    <p class="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. </p>
+                </div>
+            </div><iframe allowfullscreen="" frameborder="0" src="https://cdn.bootstrapstudio.io/placeholders/map.html" width="100%" height="450"></iframe>
+        </section> --}}
+        <div class="social"><a href="https://www.instagram.com/_noor_ul_islam"><i class="icon ion-social-instagram"></i></a><a href="https://www.snapchat.com/noorulislam770"><i class="icon ion-social-snapchat"></i></a><a href="https://www.twitter.com/noorulislam770"><i class="icon ion-social-twitter"></i></a><a href="https://www.facebook.com/noorulislam770"><i class="icon ion-social-facebook"></i></a></div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="#about">Home</a></li>
+            <li class="list-inline-item"><a href="#services">Services</a></li>
+            <li class="list-inline-item"><a href="#about">About</a></li>
+            <li class="list-inline-item"></li>
+            <li class="list-inline-item"></li>
+        </ul>
+        <p class="copyright">Company Name © 2022</p>
+    </footer>
+    <script src="{{('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+</body>
+
+</html>
